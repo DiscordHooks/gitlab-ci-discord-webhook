38cc732d745903d01ced675af6abc9570e0b27f1
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-10 11:59:58 +0530

    add send script

---
+++ send.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+if [ -z "$2" ]; then
+  echo -e "WARNING!!\nYou need to pass the WEBHOOK_URL environment variable as the second argument to this script.\nFor details & guide, visit: https://github.com/DiscordHooks/gitlab-ci-discord-webhook" && exit
+fi
+
+echo -e "[Webhook]: Sending webhook to Discord...\\n";
+
+case $1 in
+  "success" )
+    EMBED_COLOR=3066993
+    STATUS_MESSAGE="Passed"
+    ;;
+
+  "failure" )
+    EMBED_COLOR=15158332
+    STATUS_MESSAGE="Failed"
+    ;;
+
+  * )
+    EMBED_COLOR=0
+    STATUS_MESSAGE="Status Unknown"
+    ;;
+esac
+
+AUTHOR_NAME="$(git log -1 "$CI_COMMIT_SHA" --pretty="%aN")"
+COMMITTER_NAME="$(git log -1 "$CI_COMMIT_SHA" --pretty="%cN")"
+COMMIT_SUBJECT="$(git log -1 "$CI_COMMIT_SHA" --pretty="%s")"
+COMMIT_MESSAGE="$(git log -1 "$CI_COMMIT_SHA" --pretty="%b")" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g'
+
+if [ "$AUTHOR_NAME" == "$COMMITTER_NAME" ]; then
+  CREDITS="$AUTHOR_NAME authored & committed"
+else
+  CREDITS="$AUTHOR_NAME authored & $COMMITTER_NAME committed"
+fi
+
+if [ -z $CI_MERGE_REQUEST_ID ]; then
+  URL=""
+else
+  URL="$CI_PROJECT_URL/merge_requests/$CI_MERGE_REQUEST_ID"
+fi
+
+TIMESTAMP=$(date --utc +%FT%TZ)
+WEBHOOK_DATA='{
+  "username": "",
+  "avatar_url": "https://gitlab.com/favicon.png",
+  "embeds": [ {
+    "color": '$EMBED_COLOR',
+    "author": {
+      "name": "Pipeline #'"$CI_PIPELINE_IID"' '"$STATUS_MESSAGE"' - '"$CI_PROJECT_PATH_SLUG"'",
+      "url": "'"$CI_PIPELINE_URL"'",
+      "icon_url": "https://gitlab.com/favicon.png"
+    },
+    "title": "'"$COMMIT_SUBJECT"'",
+    "url": "'"$URL"'",
+    "description": "'"${COMMIT_MESSAGE//$'\n'/ }"\\n\\n"$CREDITS"'",
+    "fields": [
+      {
+        "name": "Commit",
+        "value": "'"[\`$CI_COMMIT_SHORT_SHA\`]($CI_PROJECT_URL/commit/$CI_COMMIT_SHA)"'",
+        "inline": true
+      },
+      {
+        "name": "Branch",
+        "value": "'"[\`$CI_COMMIT_REF_NAME\`]($CI_PROJECT_URL/tree/$CI_COMMIT_REF_NAME)"'",
+        "inline": true
+      }
+    ],
+    "timestamp": "'"$TIMESTAMP"'"
+  } ]
+}'
+
+(curl --fail --progress-bar -A "GitLabCI-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "$WEBHOOK_DATA" "$2" \
+  && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
